7b1ffa786d2904f9de56f0fd5b749d61035f9bff
arslonga83 <jlwood83@gmail.com>
2022-11-29 20:53:08 -0800

    start moving boilerplate to script

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 import { menuArray } from '/data.js'
 
+const orderArray = [];
+
 document.addEventListener('click', (e) => {
   if (e.target.dataset.item) {
     addMenuItem(e.target.dataset.item)
@@ -25,18 +27,47 @@ function getMenuHtml() {
   return menuHtml;
 }
 
+function getOrderHtml() {
+  let orderHtml = ''
+  orderHtml = `
+      <h1 class="order-title">Your order</h1>
+      <div class="order-line">
+        <h2>Pizza</h2>
+        <p>remove</p>
+        <h2 class="price">$14</h2>
+      </div>
+      <div class="order-line">
+        <h2>Beer</h2>
+        <p>remove</p>
+        <h2 class="price">$12</h2>
+      </div>
+      <div class="order-line total-line">
+        <h2>Total price:</h2>
+        <h2 class="price">$12</h2>
+      </div>
+      <button class="order-btn">Complete order</button>
+  `
+  return orderHtml
+}
+
 
 function addMenuItem(itemId) {
-  const menuItem = menuArray.filter((item) => {
+  orderArray.push(menuArray.filter((item) => {
     return item.id == itemId
-  })[0]
-  console.log(menuItem)
+  })[0])
+  console.log(orderArray)
+  renderOrder()
 }
 
 
 function render() {
   const menuContainer = document.querySelector('#menu-container');
   menuContainer.innerHTML = getMenuHtml();
+}
+
+function renderOrder() {
+  const orderContainer = document.querySelector('#order-container')
+  orderContainer.innerHTML = getOrderHtml();
 }
 
 render()
